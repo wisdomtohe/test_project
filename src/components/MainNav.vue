ecd781c544d67c56af5f366902b9cf44a3dac1fc
Wisdom Tohe <elmanciowisdom@gmail.com>
2020-02-02 18:53:24 +0000

    committing start-up files

---
+++ src/components/MainNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+   <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img alt="Vue logo" src="../assets/images/menu.png">
+    </a>
+  </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      image: '../assets/images/menu.png'
+    }
+  }
+}
+</script>
